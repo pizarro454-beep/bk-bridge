628d726e0cf72780c6c00f01f1ea35357c8210c4
pizarro454-beep <pizarro454@gmail.com>
2025-11-16 15:49:31 -0500

    modified subheader

--- streamlit-temporal-local.py
+++ streamlit-temporal-local.py
@@ -10,7 +10,7 @@ bridge = pd.read_csv(filename,
                      parse_dates=True,
                      index_col='hour_beginning')
 
-st.subheader('Jeffrey Pizarro Brooklyn Bridge Pedestrian Crossings')
+st.subheader('Brooklyn Bridge Pedestrian Crossings - Jeffrey Pizarro')
 st.markdown('---')
 volume = st.radio("Pedestrian Traffic Volume",('Hourly', 'Daily', 'Weekly'))
 st.markdown('---')
